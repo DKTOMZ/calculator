cd840eabd3383541f1d45bacb6afeabc5ce9c93e
Dennis Tomno <deni.tomno@gmail.com>
2023-02-05 02:45:43 +0300

    UI fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { useEffect, useRef, useState } from "react";
-import {create,all, isNumber,} from "mathjs";
+import {create,all} from "mathjs";
 import "./App.scss";
 
 export const App = () => {
     const math = create(all,{});
-    const [inputScreen, setInputScreen] = useState('');
+    const [inputScreen, setInputScreen] = useState(' ');
     const [outputScreen, setOutputScreen] = useState(0);
     const [ans, setAns] = useState(0);
     const [isDecimal, setIsDecimal] = useState(true);
     const [power, setPower] = useState('ON');
     const [equalsPressed, setEqualsPressed] = useState(false);
-    const [ACPressed, setACPressed] = useState(false);
     const normalButtons = [
         {id:'seven', name: '7', value: '7'},
         {id:'eight', name:'8', value: '8'},
@@ -50,6 +49,19 @@ export const App = () => {
         {id:')', name:')'},
         {id:'F<=>D', name:'F<=>D'}
     ];
+    const specialFunctions = [
+        'sin(',
+        'cos(',
+        'tan(',
+        'abs(',
+        'sqrt',
+        'pow(',
+        'cbrt(',
+        ', ',
+        'log(',
+        'log10(',
+        'log2('
+    ];
 
     const inputRef = useRef();
     const outputRef = useRef();
@@ -65,26 +77,43 @@ export const App = () => {
     
     const updateInputScreen = (button) => {
         setEqualsPressed(false);
-        setACPressed(false);
         if (button.value === 'DEL' && inputScreen !== ''){
-            setInputScreen(inputScreen.substring(0,(inputScreen.length-1)));
+            let split = inputScreen.substring(0,inputRef.current.selectionStart).split(' ').filter((value,index,string)=>value!=='');
+            if (specialFunctions.includes(split[split.length-1])) {
+                let toRemove = inputRef.current.selectionStart-split[split.length-1].length;
+                setInputScreen(inputScreen.replace(split[split.length-1],''));
+                setTimeout(() => {
+                    adjustCaretAfterEdit(toRemove);
+                  }, 0);
+            }
+            else {
+                let toRemove = inputRef.current.selectionStart-1;
+                setInputScreen(inputScreen.substring(0,inputRef.current.selectionStart-1)+inputScreen.substring(inputRef.current.selectionStart-1+1));
+                setTimeout(() => {
+                    adjustCaretAfterEdit(toRemove);
+                  }, 0);
+            }
         } 
         else if (button.name === 'Ans') {setInputScreen(ans.toString())} 
         else if (button.value === 'AC'){
-            setACPressed(true);
-            setInputScreen('0');
+            setInputScreen('');
             setOutputScreen(0);
         } 
         else if (button.value === '=' || button.value === 'DEL') {}
-        else if(['+','-','*','/'] && equalsPressed){setInputScreen(`${ans}${button.value}`)} 
+        else if(['+','-','*','/'].includes(button.value) && equalsPressed){setInputScreen(`${ans}${button.value}`)} 
         else {
             let currentFormula = inputScreen.split(/[\s\+\-\*\/]/);
             if (inputScreen.charAt(inputScreen.length-1)==='0' && inputScreen.length === 1 && ['0','1','2','3','4','5','6','7','8','9'].includes(button.value)){} 
             
             if(button.value==='.' && (inputScreen.charAt(inputScreen.length-1)==='.' || currentFormula[currentFormula.length-1].includes('.'))){}
             
-            if (inputScreen.length ===1 && inputScreen==='0' && ACPressed){setInputScreen(button.value)}
-            else {setInputScreen(inputScreen+button.value);}
+            else {
+                let toAdd = inputRef.current.selectionStart+1;
+                setInputScreen(inputScreen.substring(0,inputRef.current.selectionStart)+button.value+inputScreen.substring(inputRef.current.selectionStart));
+                setTimeout(() => {
+                    adjustCaretAfterEdit(toAdd);
+                  }, 0);
+            }
         }
         inputRef.current.focus();
     };
@@ -93,17 +122,6 @@ export const App = () => {
         setEqualsPressed(true);
         let exprList = inputScreen.split(' ').filter((value,index,string)=>value!=='');
         const newExprList = exprList.map((expr,index,array)=>{
-            let multipleOperators = expr.match(/\d+[\+\*\/]{2,}/g);
-            let normalOperators = expr.replace(/\d+[\+\*\/]{2,}/g,'');
-            if (multipleOperators) {
-                let operators = multipleOperators.map((value)=>{
-                    return value.match(/[\+\-\*\/]/g).join('');
-                });
-                let ans = multipleOperators.map((value,index)=>{
-                    return value.match(/\d+/g)+operators[index].substring(operators[index].length-1);
-                }).join('');
-                expr = ans+normalOperators;
-            }
             let start = expr; let end = '';
             if (start.match(/sin/) != null) {
                 end += start.replace(/sin/g,'Math.sin');
@@ -211,25 +229,86 @@ export const App = () => {
             ans = error.name.replace('E',' E');
         }
         setOutputScreen(ans);
-        setInputScreen(ans);
         setIsDecimal(true);
         if (!isNaN(ans) && isFinite(ans)) {
             updateAnsVariable(ans);
         }
     }
 
+    const adjustCaretAfterEdit = (pos) => {
+        inputRef.current.focus();
+        let caretPos = pos < 1 ? 1: pos;
+        if(inputRef.current.createTextRange) {
+            var range = inputRef.current.createTextRange();
+            range.move('character', caretPos);
+            range.select();
+        }
+        else {
+            if(inputRef.current.selectionStart) {
+                inputRef.current.focus();
+                inputRef.current.setSelectionRange(caretPos, caretPos);
+            }
+            else
+                inputRef.current.focus();
+        }
+    };
+    
+    const handleLeftNavButton = () => {
+        inputRef.current.focus();
+        let caretPos = inputRef.current.selectionStart === 1 ? 1 : inputRef.current.selectionStart-1;
+        if(inputRef.current.createTextRange) {
+            var range = inputRef.current.createTextRange();
+            range.move('character', caretPos);
+            range.select();
+        }
+        else {
+            if(inputRef.current.selectionStart) {
+                inputRef.current.focus();
+                inputRef.current.setSelectionRange(caretPos, caretPos);
+            }
+            else
+                inputRef.current.focus();
+        }
+    };
+
+    const handleRightNavButton = () => {
+        inputRef.current.focus();
+        let caretPos = inputRef.current.selectionStart+1;
+        if(inputRef.current.createTextRange) {
+            var range = inputRef.current.createTextRange();
+            range.move('character', caretPos);
+            range.select();
+        }
+        else {
+            if(inputRef.current.selectionStart) {
+                inputRef.current.focus();
+                inputRef.current.setSelectionRange(caretPos, caretPos);
+            }
+            else
+                inputRef.current.focus();
+        }
+        if (caretPos === 0) {inputRef.current.selectionStart = 1}
+    };
+
     return (
         <div className="App">
             <div className="calculator">
-                <h2>SCICALC</h2>
-                <h3>VALHALLA</h3>
+                <h2>SCICALC <p>By <a href="https://dennis-tomno.onrender.com" rel="noreferrer" target="_blank">Dennis Tomno</a></p></h2>
                 <div className="input-screen">
-                    <input id="display" ref={inputRef} type="text" onKeyDown={(e)=>e.preventDefault()} onChange={(e)=>setInputScreen(e.target.value)} value={inputScreen}/>
+                    <input ref={inputRef} type="text" onKeyDown={(e)=>e.preventDefault()} onChange={(e)=>setInputScreen(e.target.value)} value={inputScreen}/>
                 </div>
                 <div className="output-screen">
-                    <input onKeyDown={(e)=>e.preventDefault()} ref={outputRef} onChange={(e)=>setInputScreen(e.target.value)} value={outputScreen}/>
+                    <input id="display" onKeyDown={(e)=>e.preventDefault()} ref={outputRef} onChange={(e)=>setInputScreen(e.target.value)} value={outputScreen}/>
                 </div>
                 <div className="buttons">
+                    <div className="nav">
+                        <div className="nav-left" onClick={handleLeftNavButton}>
+                            <p><i className="arrow left"></i></p>
+                        </div>
+                        <div className="nav-left" onClick={handleRightNavButton}>
+                            <p><i className="arrow right"></i></p>
+                        </div>
+                    </div>
                     <div className="special">
                         {specialButtons.map((button, index, array)=>{
                             return <div style={[',','³√','(',')'].includes(button.name)?{fontSize:'22px'}:{}} key={button.id} id={button.id} onClick={()=>handleSpecialButtons(button)} className="special-button">{button.name}</div>
